646720fbe02fe97dc847fc195a192d0105424ee0
nagybrandy <57422699+nagybrandy@users.noreply.github.com>
2023-02-27 19:15:59 +0100

    1. gyakorlat

--- kliensoldali 4. csoport/progressive_templates (1)/js/task3.js
+++ kliensoldali 4. csoport/progressive_templates (1)/js/task3.js
@@ -1,16 +1,15 @@
 const observer = new IntersectionObserver(onObserve, {
-  threshold: [0.1, 0.2, 0.3, 0.4, 0.5, 1.0],
+  threshold: 1.0,
 })
 
-function onObserve(entries) {
-  entries.forEach(e => console.log(e.target))
+function onObserve(entries){
   entries.forEach(entry => {
-    if(entry.isIntersecting){
+    if(entry.isIntersecting) {
       entry.target.classList.add(
         'animate__animated',
-        `animate__${entry.target.dataset.dataScrollAnimation}`)
+        `animate__${entry.target.dataset.scrollAnimation}`
+      )
     }
   })
 }
-
 document.querySelectorAll('[data-scroll]').forEach(e => observer.observe(e))
